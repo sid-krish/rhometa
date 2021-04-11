7d302ddbc6e916ca13fc3ba5ae12f988bc4d8620
Sid-Kr1 <56985355+Sid-Kr1@users.noreply.github.com>
2021-04-11 15:24:31 +1000

    Update final_results.py

--- bin/final_results.py
+++ bin/final_results.py
@@ -29,13 +29,13 @@ df.sort_values(by="rho_for_estimator", inplace=True)
 #     theta = file.readline()
 
 with open("final_results.txt", 'w') as file:  # open in write mode (create new file)
-    file.write(f"Custom Pairwise Recombination Rate Estimator\n")
+    file.write(f"Custom Metagenome Pairwise Recombination Rate Estimator\n")
     file.write(f"\n")
 
-    file.write(f'-' * 45 + '\n')
+    file.write(f'-' * 55 + '\n')
     file.write(f"Theta estimate = {theta}\n")
     file.write(f"Max rho = {max_rho}, Max lk = {max_lk}\n")
-    file.write(f'-' * 45 + '\n')
+    file.write(f'-' * 55 + '\n')
 
     file.write(f"\n")
     df.to_csv(file, mode = 'a', index = None)  # open in append mode (add to new file)
