97234cf41883f0e1b7f58d86ee23393dc33b177d
Sid-Kr1 <56985355+Sid-Kr1@users.noreply.github.com>
2021-12-07 16:06:20 +1100

    scripts for collecting and plotting estimated theta and recom results

---
+++ bin/aggregate_recom_results.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys
+
+import pandas as pd
+
+collected_results_csv = sys.argv[1]
+
+# collected_results_csv = "/Users/Sid/Documents/GitHub/metagenome_pop_recom_rate/s_pnemonia/Recom_Est_Output/ERR025203_Aligned_final_results_summary.csv"
+
+results_list = []
+with open(collected_results_csv, 'r') as results:
+    for line in results:
+        if line.startswith("max_rho"):
+            results_list.append(line.strip().split(","))
+
+
+df_cols = "max_rho,bootstrap_samples,mean,std,min,5%,25%,50%,75%,95%,max".split(",")
+results_df = pd.DataFrame(results_list,columns=df_cols)
+
+results_df.to_csv("recom_results.csv", index=False)
