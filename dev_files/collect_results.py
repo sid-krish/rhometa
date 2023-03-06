2977f97b86fb7bf17af01b66779b2739992dc89f
sid-krish <56985355+sid-krish@users.noreply.github.com>
2023-03-06 16:49:36 +1100

    collect results script update

--- dev_files/collect_results.py
+++ dev_files/collect_results.py
@@ -7,19 +7,21 @@ import seaborn as sns
 def theta_results(theta_results_path):
     csv_files = glob.glob(theta_results_path + "*.csv")
 
-    sample_list = [str(file).split('/')[-1].split('_')[0] for file in csv_files]
-    id_list = [str(file).split('/')[-1].split('_')[1] for file in csv_files]
+    reference_list = [str(file).split('/')[-1].split('_')[-5]
+                      for file in csv_files]
+    id_list = [str(file).split('/')[-1].split('_')[0] for file in csv_files]
 
-    df_list = [pd.read_csv(file, skiprows=2, header=None).T.drop(index=0) for file in csv_files]
+    df_list = [pd.read_csv(file, skiprows=2, header=None).T.drop(
+        index=0) for file in csv_files]
     combined_df = pd.concat(df_list, ignore_index=True)
 
     cols = ["mean_depth", "tps_mean_depth", "median_depth", "tps_median_depth"]
     combined_df.columns = cols
-    combined_df["sample"] = sample_list
+    combined_df["reference"] = reference_list
     combined_df["identifier"] = id_list
 
     combined_df = combined_df.reindex(
-        columns=["sample", "identifier", "mean_depth", "tps_mean_depth", "median_depth", "tps_median_depth"])
+        columns=["reference", "identifier", "mean_depth", "tps_mean_depth", "median_depth", "tps_median_depth"])
     combined_df.to_csv("collected_theta_results.csv", index=False)
 
     sns.set_palette("Set1")
@@ -32,19 +34,21 @@ def theta_results(theta_results_path):
 def rho_results(rho_results_path):
     csv_files = glob.glob(rho_results_path + "*estimate.csv")
 
-    sample_list = [str(file).split('/')[-1].split('_')[0] for file in csv_files]
-    seed_list = [str(file).split('/')[-1].split('_')[1] for file in csv_files]
-    id_list = [str(file).split('/')[-1].split('_')[2] for file in csv_files]
+    reference_list = [str(file).split('/')[-1].split('_')[-4]
+                      for file in csv_files]
+    seed_list = [str(file).split('/')[-1].split('_')[0] for file in csv_files]
+    id_list = [str(file).split('/')[-1].split('_')[1] for file in csv_files]
 
     df_list = [pd.read_csv(file) for file in csv_files]
 
     combined_df = pd.concat(df_list, ignore_index=True)
 
-    combined_df["sample"] = sample_list
+    combined_df["reference"] = reference_list
     combined_df["seed"] = seed_list
     combined_df["identifier"] = id_list
 
-    combined_df = combined_df.reindex(columns=['sample', 'identifier', 'seed', 'rho', 'log_likelihood_sum'])
+    combined_df = combined_df.reindex(
+        columns=['reference', 'identifier', 'seed', 'rho', 'log_likelihood_sum'])
     combined_df.to_csv("collected_rho_results.csv", index=False)
 
     sns.set_palette("Set2")
@@ -61,5 +65,6 @@ if __name__ == '__main__':
     theta_combined_df = theta_results(theta_results_path)
     rho_combined_df = rho_results(rho_results_path)
 
-    merged_df = pd.merge(rho_combined_df, theta_combined_df, on='identifier').drop(columns=["sample_y"])
+    merged_df = pd.merge(rho_combined_df, theta_combined_df,
+                         on='identifier').drop(columns=["reference_y"])
     merged_df.to_csv("collected_merged_results.csv", index=False)
