3909fabcfccdb27819ad0c861f6609cc2a6740dd
sid-krish <56985355+sid-krish@users.noreply.github.com>
2023-09-15 14:46:06 +1000

    Update hpc_node_run.sh

--- dev_files/hpc_node_run.sh
+++ dev_files/hpc_node_run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #PBS -M 11849395@uts.edu.au -m abe
-#PBS -l select=1:ncpus=12:mem=20GB:host=i3node01 -l walltime=120:00:00 -q i3q
+#PBS -l select=1:ncpus=12:mem=30GB -l walltime=120:00:00 -q i3q
 
 cd $PBS_O_WORKDIR
 
-nextflow run sub_theta_align_est.nf -qs 50 -resume
+source $HOME/miniconda3/etc/profile.d/conda.sh
+conda activate
+
+nextflow run combined_align_est.nf -qs 50 -resume
