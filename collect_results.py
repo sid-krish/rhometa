3fe70606615266d44036fea30f51d687a7d581b1
sid-krish <56985355+sid-krish@users.noreply.github.com>
2024-05-20 14:38:21 +1000

    collect_results.py more in line with ncbi samples and references

--- collect_results.py
+++ collect_results.py
@@ -3,8 +3,6 @@ import glob
 import numpy as np
 import pandas as pd
 
-# import seaborn as sns
-
 
 def cov_results(cov_results_path):
     # Check sort order
@@ -52,12 +50,6 @@ def cov_results(cov_results_path):
     )
     combined_df.to_csv("collected_depth_results.csv", index=False)
 
-    # sns.set_palette("Set1")
-    # fig = sns.boxplot(combined_df, x="weighted_mean_depth")
-    # fig.set_xlim(1, 100)
-    # fig.figure.savefig("weighted_mean_depth_results_plot.png", dpi=500)
-    # fig.figure.clf()
-
     return combined_df
 
 
@@ -65,21 +57,14 @@ def theta_results(theta_results_path):
     # Check sort order
     csv_files = glob.glob(theta_results_path + "*.csv")
 
-    reference_list = [str(file).split("/")[-1].split("_")[-5] for file in csv_files]
+    reference_list = [str(file).split("/")[-1].split("_")[1] + '_' + str(file).split("/")[-1].split("_")[2] for file in csv_files]
     id_list = [str(file).split("/")[-1].split("_")[0] for file in csv_files]
 
     df_list = [
-        pd.read_csv(file, skiprows=2, header=None).T.drop(index=0) for file in csv_files
+        pd.read_csv(file) for file in csv_files
     ]
     combined_df = pd.concat(df_list, ignore_index=True)
 
-    cols = [
-        "pileup_mean_depth",
-        "tps_pileup_mean_depth",
-        "pileup_median_depth",
-        "tps_pileup_median_depth",
-    ]
-    combined_df.columns = cols
     combined_df["reference"] = reference_list
     combined_df["identifier"] = id_list
 
@@ -88,17 +73,14 @@ def theta_results(theta_results_path):
             "reference",
             "identifier",
             "pileup_mean_depth",
-            "tps_pileup_mean_depth",
             "pileup_median_depth",
-            "tps_pileup_median_depth",
+            "theta_per_site_mean_depth",
+            "theta_per_site_median_depth",
+            "genome_length"
         ]
     )
-    combined_df.to_csv("collected_theta_results.csv", index=False)
 
-    # sns.set_palette("Set2")
-    # fig = sns.boxplot(combined_df, x="tps_pileup_mean_depth")
-    # fig.figure.savefig("theta_results_plot.png", dpi=500)
-    # fig.figure.clf()
+    combined_df.to_csv("collected_theta_results.csv", index=False)
 
     return combined_df
 
@@ -107,7 +89,7 @@ def rho_results(rho_results_path):
     # Check sort order
     csv_files = glob.glob(rho_results_path + "*estimate.csv")
 
-    reference_list = [str(file).split("/")[-1].split("_")[-4] for file in csv_files]
+    reference_list = [str(file).split("/")[-1].split("_")[2] + '_' + str(file).split("/")[-1].split("_")[3]  for file in csv_files]
     seed_list = [str(file).split("/")[-1].split("_")[0] for file in csv_files]
     id_list = [str(file).split("/")[-1].split("_")[1] for file in csv_files]
 
@@ -120,15 +102,10 @@ def rho_results(rho_results_path):
     combined_df["identifier"] = id_list
 
     combined_df = combined_df.reindex(
-        columns=["reference", "identifier", "seed", "rho", "log_likelihood_sum"]
+        columns=["reference", "identifier", "seed", "rho","log_likelihood_sum",
+                 "recom_tract_len","rho_per_site"]
     )
     combined_df.to_csv("collected_rho_results.csv", index=False)
-
-    # sns.set_palette("Set3")
-    # fig = sns.boxplot(combined_df, x="rho")
-    # fig.set_xlim(1, 100)
-    # fig.figure.savefig("rho_results_plot.png", dpi=500)
-    # fig.figure.clf()
 
     return combined_df
 
@@ -163,32 +140,32 @@ def subsample_results(subsample_results_path):
 
 
 if __name__ == "__main__":
-    cov_results_path = ""
+    # cov_results_path = ""
     theta_results_path = ""
     rho_results_path = ""
-    subsample_results_path = ""
+    # subsample_results_path = ""
 
-    cov_combined_df = cov_results(cov_results_path)
+    # cov_combined_df = cov_results(cov_results_path)
     theta_combined_df = theta_results(theta_results_path)
     rho_combined_df = rho_results(rho_results_path)
-    subsample_results_df = subsample_results(subsample_results_path)
+    # subsample_results_df = subsample_results(subsample_results_path)
 
-    cov_theta_merged_df = pd.merge(
-        cov_combined_df, theta_combined_df, on=["reference", "identifier"]
-    )
+    # cov_theta_merged_df = pd.merge(
+    #     cov_combined_df, theta_combined_df, on=["reference", "identifier"]
+    # )
 
     cov_theta_rho_merged_df = pd.merge(
-        cov_theta_merged_df, rho_combined_df, on=["reference", "identifier"]
+        theta_combined_df, rho_combined_df, on=["reference", "identifier"]
     )
 
-    cov_theta_rho_sub_merged_df = pd.merge(
-        cov_theta_rho_merged_df,
-        subsample_results_df,
-        on=["reference", "identifier", "seed"]
-    )
+    # cov_theta_rho_sub_merged_df = pd.merge(
+    #     cov_theta_rho_merged_df,
+    #     subsample_results_df,
+    #     on=["reference", "identifier", "seed"]
+    # )
 
-    cov_theta_rho_sub_merged_df = cov_theta_rho_sub_merged_df.sort_values(
-        by=["reference", "identifier", "seed"]
-    )
+    # cov_theta_rho_sub_merged_df = cov_theta_rho_sub_merged_df.sort_values(
+    #     by=["reference", "identifier", "seed"]
+    # )
 
-    cov_theta_rho_sub_merged_df.to_csv("collected_merged_results.csv", index=False)
+    cov_theta_rho_merged_df.to_csv("collected_merged_results.csv", index=False)
